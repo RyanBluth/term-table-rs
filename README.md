a7f8f04c22fd300c4bce02f4ae38b8178c317257
Ryan Bluth <ryanbluth93@gmail.com>
2018-05-13 15:46:03 -0400

    Added basic readme

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # term-table-rs
 
 ![example](https://i.imgur.com/XwIzWkU.png)
+
+The purpose of term-table-rs is to make displaying table data in CLI apps easier
+
+### Here is an example of how to create a table
+
+```rust
+let mut table = Table::new();
+table.max_column_width = 40;
+
+table.style = TableStyle::extended(); 
+
+table.add_row(Row::new(vec![
+    Cell::new_with_alignment("This is some centered text", 2, Alignment::Center)
+])); 
+
+table.add_row(Row::new(vec![
+    Cell::new("This is left aligned text", 1),
+    Cell::new_with_alignment("This is right aligned text", 1, Alignment::Right)
+]));
+
+    table.add_row(Row::new(vec![
+    Cell::new("This is left aligned text", 1),
+    Cell::new_with_alignment("This is right aligned text", 1, Alignment::Right)
+]));
+
+table.add_row(Row::new(vec![
+    Cell::new("This is some really really really really really really really really really that is going to wrap to the next line", 2),
+]));   
+
+println!("{}", table.as_string());
+
+```
+### Here's the result
+
+<pre>
+ ╔═════════════════════════════════════════════════════════════════════════════════╗
+ ║                            This is some centered text                           ║
+ ╠════════════════════════════════════════╦════════════════════════════════════════╣
+ ║ This is left aligned text              ║             This is right aligned text ║
+ ╠════════════════════════════════════════╬════════════════════════════════════════╣
+ ║ This is left aligned text              ║             This is right aligned text ║
+ ╠════════════════════════════════════════╩════════════════════════════════════════╣
+ ║ This is some really really really really really really really really really tha ║
+ ║ t is going to wrap to the next line                                             ║
+ ╚═════════════════════════════════════════════════════════════════════════════════╝
+</pre>
--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,23 @@
 //!# Example
 //! Here is an example of how to create a simple table
 //!```
-//!let mut table = Table::new();
+//!let mut table = term_table::Table::new();
 //!table.max_column_width = 40;
 //!
-//!table.style = TableStyle::extended(); 
-//!table.add_row(Row::new(vec![
-//!    Cell::new_with_alignment("This is some centered text", 2, Alignment::Center)
+//!table.style = term_table::TableStyle::extended(); 
+//!table.add_row(term_table::row::Row::new(vec![
+//!    term_table::cell::Cell::new_with_alignment("This is some centered text", 2, term_table::cell::Alignment::Center)
 //!])); 
-//!table.add_row(Row::new(vec![
-//!    Cell::new("This is left aligned text", 1),
-//!    Cell::new_with_alignment("This is right aligned text", 1, Alignment::Right)
+//!table.add_row(term_table::row::Row::new(vec![
+//!    term_table::cell::Cell::new("This is left aligned text", 1),
+//!    term_table::cell::Cell::new_with_alignment("This is right aligned text", 1, term_table::cell::Alignment::Right)
 //!]));
-//! table.add_row(Row::new(vec![
-//!    Cell::new("This is left aligned text", 1),
-//!    Cell::new_with_alignment("This is right aligned text", 1, Alignment::Right)
+//! table.add_row(term_table::row::Row::new(vec![
+//!    term_table::cell::Cell::new("This is left aligned text", 1),
+//!    term_table::cell::Cell::new_with_alignment("This is right aligned text", 1, term_table::cell::Alignment::Right)
 //!]));
-//!table.add_row(Row::new(vec![
-//!    Cell::new("This is some really really really really really really really really really that is going to wrap to the next line", 2),
+//!table.add_row(term_table::row::Row::new(vec![
+//!    term_table::cell::Cell::new("This is some really really really really really really really really really that is going to wrap to the next line", 2),
 //!]));   
 //!println!("{}", table.as_string());
 //!```
@@ -61,7 +61,7 @@ pub enum RowPosition {
 ///# Example
 ///
 ///```
-///   TableStyle {
+///   term_table::TableStyle {
 ///            top_left_corner: '╔',
 ///            top_right_corner: '╗',
 ///            bottom_left_corner: '╚',
